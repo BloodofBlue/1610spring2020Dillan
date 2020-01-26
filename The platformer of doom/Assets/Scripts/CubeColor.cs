37d500ccf512ea14cd826dafbff528c8e51ba924
Dillan <imadgical@gmail.com>
2020-01-25 19:33:04 -0700

    Color changing cube tutorial code
    Might have broken everything by forgetting to pull before changing stuff

---
+++ The platformer of doom/Assets/Scripts/CubeColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This code came from the scripts and behavior tutorial on learn.unity.com. Except the last little bit, I added that myself.
+public class CubeColor : MonoBehaviour
+{ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            print("fart");
+        }
+    }
+}
