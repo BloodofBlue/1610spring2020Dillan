4be47c0dafbaf3eb47f947a90e7ed893e45cfb67
Dillan <imadgical@gmail.com>
2020-01-27 11:18:15 -0700

    Trigger lecture

---
+++ The platformer of doom/Assets/Scripts/TriggerEvents.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvents : MonoBehaviour
+{
+    public UnityEvent triggerEnterExitEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterExitEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("Get the fuck outta here m80");
+    }
+}
