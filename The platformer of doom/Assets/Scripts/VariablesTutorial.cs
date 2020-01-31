d16e22f3bd1699ff35d03e828f3a5e2127623e4d
Dillan <imadgical@gmail.com>
2020-01-30 23:59:26 -0700

    Unity tutorial

---
+++ The platformer of doom/Assets/Scripts/VariablesTutorial.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class VariablesTutorial : MonoBehaviour
+{
+    private int myInt = 5;
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log();
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        Int ret;
+        ret = number * 2;
+        return ret;
+    }
+    
+    //THis code came from a tutorial on the unity website, will cite specific one later.
+    
+    //it was the second one
+}    
