2a16ad032b4e09986dd680f4b4362c2ea484812b
Dillan <imadgical@gmail.com>
2020-02-06 23:34:25 -0700

    Started on a proper 2d platformer

---
+++ The platformer of doom/Assets/Scripts/TwoDimensionalPLayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class TwoDimensionalPLayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float characterSpeed = 5f;
+    public float gravity = -1f;
+    public float jumpForce = 15f;
+    void Start()
+    {
+        
+
+    }
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*characterSpeed;
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+        }
+        
+        positionDirection.y += gravity;
+        
+        controller.Move(positionDirection*Time.deltaTime);
+
+    }
+
+
+}
