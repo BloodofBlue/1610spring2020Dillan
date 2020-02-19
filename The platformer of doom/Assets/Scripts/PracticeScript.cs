db9d9e7a721494b3ccdf13db1e2e4e317921ad22
Bobsicle <Imadgical@gmail.com>
2020-02-18 23:44:21 -0700

    Rider decided to break, trashed one script and half made another
    I had to use the time I set aside to code trying to get rider to stop malfunctioning. >:U

---
+++ The platformer of doom/Assets/Scripts/PracticeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeScript : MonoBehaviour
+{
+    public float practicenumber;
+    void Start()
+    {
+        if
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
