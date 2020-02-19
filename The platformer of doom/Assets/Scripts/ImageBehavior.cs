af489e81348031cc3fd7a9501f04072bd5f2e481
Dillan <imadgical@gmail.com>
2020-02-19 11:16:08 -0700

    Scriptable objects lecture

---
+++ The platformer of doom/Assets/Scripts/ImageBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageBehavior : MonoBehaviour
+{
+
+    private Image imageObj;
+
+    private void Start()
+    {
+        imageObj = GetComponent<Image>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
