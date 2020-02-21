88f1a425ac720891edcf3b1726a3bd044592cecc
Bobsicle <Imadgical@gmail.com>
2020-02-20 23:02:43 -0700

    trigger practice

---
+++ The platformer of doom/Assets/Scripts/New Folder/TriggerPracticeDeleteMeBeforeThingsBreak.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPracticeDeleteMeBeforeThingsBreak : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerExitEvent;
+    public UnityEvent triggerStayEvent;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+        print("ohai there");
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+    triggerExitEvent.Invoke();
+    print("See ya later bucko");
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+    triggerStayEvent.Invoke();
+    print("AAAAAAAAAAAAAA");
+    }
+}
