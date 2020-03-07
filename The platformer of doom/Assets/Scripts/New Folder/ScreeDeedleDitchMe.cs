d1eda03df582af89fb5c00ae6583858a307e151a
Bobsicle <Imadgical@gmail.com>
2020-03-06 23:45:51 -0700

    practice

---
+++ The platformer of doom/Assets/Scripts/New Folder/ScreeDeedleDitchMe.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+[RequireComponent(typeof(CharacterController))]
+public class TwoDimensionalPLayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 positionDirection;
+    public float characterSpeed = 5f;
+    public float gravity = -1f;
+    public float jumpForce = 15f;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*characterSpeed;
+        
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+        }
+        
+        positionDirection.y += gravity;
+        
+        controller.Move(positionDirection*Time.deltaTime);
+
+
+    }
+
+
+}
