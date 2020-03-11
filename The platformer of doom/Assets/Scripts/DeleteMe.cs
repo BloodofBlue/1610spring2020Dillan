0105f042665ce5b2ea11eb0cefac193fa7b01f64
Bobsicle <Imadgical@gmail.com>
2020-03-10 22:54:08 -0600

    practice and cleanup

---
+++ The platformer of doom/Assets/Scripts/DeleteMe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DeleteMe : MonoBehaviour
+{
+    public int triggeringInteger;
+    public float triggeringFloat;
+    public string triggeringString;
+
+    public UnityEvent enterTheVortex;
+    public UnityEvent exitTheVortex;
+    public UnityEvent stayInTheVortex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enterTheVortex.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        exitTheVortex.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        stayInTheVortex.Invoke();
+    }
+}
