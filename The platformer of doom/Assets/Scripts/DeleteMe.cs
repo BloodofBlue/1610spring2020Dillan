6b87ba9d9d69feccf2eafd857ab7bcd2c2045d9c
Bobsicle <Imadgical@gmail.com>
2020-03-26 22:43:11 -0600

    practice

--- The platformer of doom/Assets/Scripts/DeleteMe.cs
+++ The platformer of doom/Assets/Scripts/DeleteMe.cs
@@ -1,31 +1,39 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
+﻿using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
-public class DeleteMe : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class TwoDimensionalPLayerMovement : MonoBehaviour
 {
-    public int triggeringInteger;
-    public float triggeringFloat;
-    public string triggeringString;
-
-    public UnityEvent enterTheVortex;
-    public UnityEvent exitTheVortex;
-    public UnityEvent stayInTheVortex;
-
-    private void OnTriggerEnter(Collider other)
+    private CharacterController controller;
+    private Vector3 positionDirection;
+    public float characterSpeed = 5f;
+    public float gravity = -1f;
+    public float jumpForce = 15f;
+    void Start()
     {
-        enterTheVortex.Invoke();
+        controller = GetComponent<CharacterController>();
+
+    }
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*characterSpeed;
+        
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+        }
+        
+        positionDirection.y += gravity;
+        
+        controller.Move(positionDirection*Time.deltaTime);
+
+
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        exitTheVortex.Invoke();
-    }
 
-    private void OnTriggerStay(Collider other)
-    {
-        stayInTheVortex.Invoke();
-    }
 }
