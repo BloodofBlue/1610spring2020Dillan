f42cdf06651403eaa937f64d2f7b1135a5f6e685
Bobsicle <Imadgical@gmail.com>
2020-03-28 22:21:58 -0600

    practice

---
+++ The platformer of doom/Assets/Scripts/New Folder/IScreamForIAmIdealess.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IScreamForIAmIdealess : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent;
+    public UnityEvent mouseUpEvent;
+    public UnityEvent mouseDragEvent;
+    public UnityEvent mouseOverEvent;
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.magenta
+    }
+
+    private void OnMouseEnter()
+    {
+        print("screeee");
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+        transform.Rotate(Vector3.left);
+    }
+}
