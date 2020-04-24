2b596cb0b6b01181fe444ea2e23eb243e133ea38
Bobsicle <Imadgical@gmail.com>
2020-04-24 13:08:24 -0600

    Gamestates

---
+++ The platformer of doom/Assets/Scripts/Gamestates.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Gamestates : MonoBehaviour
+{
+    public StringData gameState;
+    public UnityEvent normal, lightsOff, endGame, stateCheck;
+
+    public enum GameStates
+    {
+        Normal,
+        LightsOff,
+        Endgame,
+    }
+
+    public GameStates currentGameState;
+    // Start is called before the first frame update
+    public void ChangeStates ()
+    {
+        if (gameState.value == "normal")
+        {
+            currentGameState = GameStates.Normal;
+        }
+        else if (gameState.value == "lightsOff")
+        {
+            currentGameState = GameStates.LightsOff;
+        }
+        else if (gameState.value == "endGame")
+        {
+            currentGameState = GameStates.Endgame;
+        }
+        else
+        {
+            print("ya fucked up lad, fix the gamestate");
+        }
+    }
+    
+    void Update()
+    {
+        stateCheck.Invoke();
+        switch (currentGameState)
+        {
+            case GameStates.Normal:
+                normal.Invoke();
+                break;
+            case GameStates.LightsOff:
+                lightsOff.Invoke();
+                break;
+            case GameStates.Endgame:
+                endGame.Invoke();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
--- The platformer of doom/Assets/Scripts/PlayerStats.cs
+++ The platformer of doom/Assets/Scripts/PlayerStats.cs
@@ -1,16 +1,25 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class PlayerStats : MonoBehaviour
 {
     public FloatData health;
     public UnityEvent dead;
+    public UnityEvent playerRespawn;
     
     void Update()
     {
         if (health.value <= 0)
         {
             dead.Invoke();
+            StartCoroutine("Delay");
         }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(0.1f);
+        playerRespawn.Invoke();
     }
 }
